795f808fb1db84c54d6591333ca352737c6da041
Mayank Dhatwalia <157281438+mayankdhatwalia@users.noreply.github.com>
2024-12-18 05:42:59 +0000

    Minor update to the clock..

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function clock(){
         h = h - 12; /* Made the clock acc. to 12 hour-clock*/
     }
 
+    /* Minor update to get 00 in hours, minutes and seconds before this we get 0, 2,3.. 
+    after this chotu sa update we will be getting the 00, 01, 02.... */
+    h = (h<10) ? "0"+ h : h;
+    m = (m<10) ? "0"+ m : m;
+    s = (s<10) ? "0"+ s : s;
+
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
